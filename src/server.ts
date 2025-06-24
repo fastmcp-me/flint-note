727306dbe8782902cc46c386602a293a6fafccf4
Tim Disney <tim@disnetdev.com>
2025-06-24 10:54:28 -0700

    lint

--- src/server.ts
+++ src/server.ts
@@ -21,7 +21,7 @@ import { NoteManager } from './core/notes.ts';
 import { NoteTypeManager } from './core/note-types.ts';
 import { SearchManager } from './core/search.ts';
 import { LinkManager } from './core/links.ts';
-import { GlobalConfigManager, initializeVaultSystem } from './utils/global-config.ts';
+import { GlobalConfigManager } from './utils/global-config.ts';
 import { resolvePath, isPathSafe } from './utils/path.ts';
 import type { LinkRelationship } from './types/index.ts';
 import type { MetadataSchema } from './core/metadata-schema.ts';
@@ -861,7 +861,7 @@ export class FlintNoteServer {
     };
   };
 
-  #handleListNoteTypes = async (args: ListNoteTypesArgs) => {
+  #handleListNoteTypes = async (_args: ListNoteTypesArgs) => {
     this.#requireWorkspace();
     if (!this.#noteTypeManager) {
       throw new Error('Server not initialized');
